ffc6352af970dd3dc9a4e2c604ffca1d34838153
Chribtoper <xcstolfax@gmail.com>
2018-07-01 04:31:41 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,10 +2,11 @@ var playlist = new Object({artist: 'song title'});
 
 function updatePlaylist(playlist, artist, songtitle){
   playlist[artist] = songtitle;
-  return playlist
+  return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName){
   delete playlist[artistName];
-  return playlist
+  return playlist;
 }
+
